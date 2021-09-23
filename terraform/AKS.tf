d6063fb093380f8026fd4ef6904d10899ff98586
Richard Ostrochovský <richard.ostrochovsky@unicornuniverse.net>
2021-09-23 02:32:41 +0200

    improvements according to https://github.com/drajcan/kubernetes-academy/tree/master/terraform/aks

--- terraform/AKS.tf
+++ terraform/AKS.tf
@@ -2,66 +2,108 @@ variable "project" {
         type = string
 }
 
-variable "env" {
+variable "environment" {
         type = string
 }
 
-variable "location" {
-        type = string
-        default = "westeurope"
+locals {
+	azure_RG_name = "${var.project}-${var.environment}-AKS"
+	azure_AKS_instance0_name = "${local.azure_RG_name}-0"
 }
 
-variable "vmsize" {
+variable "azure_tenant_id" {
 	type = string
-	default = "Standard_B2S"
 }
 
-variable "nodecount" {
-	type = string
-	default = 1
-}
-
-variable "nodepool" {
-	type = string
-	default = "default" 
-}
-
-variable "identity" {
+variable "azure_identity_type" {
 	type = string
 	default = "SystemAssigned"
 }
 
+variable "azure_region" {
+        type = string
+        default = "westeurope"
+}
+
+variable "aks_node_vm_size" {
+	type = string
+	default = "Standard_B2S"
+}
+
+variable "k8s_version" {
+  type = string
+  default = "1.21.2"
+}
+
+variable "aks_auto_scaling" {
+  type = bool
+  default = "false"
+}
+
+variable "aks_node_pool" {
+	type = string
+	default = "default" 
+}
+
+variable "aks_node_count" {
+	type = string
+	default = 1
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
 provider "azurerm" {
+  tenant_id = var.azure_tenant_id
   features {}
 }
 
-resource "azurerm_resource_group" "AKS" {
-  name     = format("RG_AKS_%s_%s", var.project, var.env)
-  location = var.location
+resource "azurerm_resource_group" "AKS_instance0" {
+  name		= local.azure_RG_name
+  location	= var.azure_region
 
   tags = {
-    environment = var.env
+	Name		= local.azure_RG_name
+	Project		= var.project
+  	Environment	= var.environment
   }
 }
 
-resource "azurerm_kubernetes_cluster" "instance1" {
-  name                = format("AKS_%s_%s", var.project, var.env)
-  location            = var.location
-  resource_group_name = azurerm_resource_group.AKS.name
-  dns_prefix          = format("AKS-%s-%s", var.project, var.env)
+resource "azurerm_kubernetes_cluster" "instance0" {
+  name                = local.azure_AKS_instance0_name
+  location            = var.azure_region
+  resource_group_name = azurerm_resource_group.AKS_instance0.name
+  dns_prefix          = local.azure_AKS_instance0_name
 
   default_node_pool {
     name       = "default"
-    node_count = var.nodecount
-    vm_size    = var.vmsize
+    node_count = var.aks_node_count
+    vm_size    = var.aks_node_vm_size
+    availability_zones = [ "1", "2", "3" ]
+    enable_auto_scaling = var.aks_auto_scaling
+
+    node_labels = {
+      Project = var.project
+      Environment = var.environment
+    }
+
   }
 
   identity {
-    type = var.identity
+    type = var.azure_identity_type
   }
 
+  kubernetes_version = var.k8s_version
+
   tags = {
-    Environment = var.env
+    Name = local.azure_AKS_instance0_name
+    Project = var.project
+    Environment = var.environment
   }
 }
 
